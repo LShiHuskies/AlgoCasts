1804b15d97ebf77c0ccd6f9a93f54b262b2e658c
github username <github email address>
2018-11-07 13:14:23 -0500

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,29 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  debugger;
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+reverse('asdf');
 
 module.exports = reverse;
+
+
+// function reverse(str) {
+//   let otherStr = '';
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     otherStr += str[i];
+//   }
+//   return otherStr;
+// }
+
+
+// function reverse(str) {
+//   let reversed = '';
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
