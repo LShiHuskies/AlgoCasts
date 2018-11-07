20a69749184ba0d5626d1c8635554781b0840cf0
github username <github email address>
2018-11-07 14:25:49 -0500

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,37 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  for (let char of str) {
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
+    }
+  }
+  let values = Object.keys(chars).map(key => chars[key]);
+  values.sort((a,b) => b - a);
+  for (let ch in chars) {
+    if (chars[ch] === values[0]) {
+      return ch;
+    }
+  }
+
+}
 
 module.exports = maxChar;
+
+
+// function maxChar(str) {
+//   const chars = {};
+//   for (let char of str) {
+//     if (!chars[char]) {
+//       chars[char] = 1;
+//     } else {
+//       chars[char]++;
+//     }
+//   }
+//
+//
+// }
