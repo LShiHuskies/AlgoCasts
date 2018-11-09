7073c21aa8606a90a0f13774ebce769d8cdf5f4b
github username <github email address>
2018-11-09 18:15:31 -0500

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,37 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while(index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
+
+
+
+// function chunk(array, size) {
+//   let b = 0;
+//   let bigArr = [];
+//   let otherCount = 0;
+//   bigArr[otherCount] = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (b < size) {
+//       bigArr[otherCount].push(array[i])
+//       b++;
+//     } else {
+//       b = 1;
+//       otherCount++;
+//       bigArr[otherCount] = [];
+//       bigArr[otherCount].push(array[i]);
+//     }
+//   }
+//   return bigArr;
+// }
