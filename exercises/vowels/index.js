e8ba91dead9cdc99f105827d6e85daa7b86d1d0b
github username <github email address>
2018-11-12 00:18:55 -0500

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,21 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0
+}
 
 module.exports = vowels;
+
+
+// function vowels(str) {
+//   let count = 0;
+//   let checkers = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'];
+//   for (let i = 0; i < str.length; i++) {
+//     if (checkers.includes(str[i]) === true) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
